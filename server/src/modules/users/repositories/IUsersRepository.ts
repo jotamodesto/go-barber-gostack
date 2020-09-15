682a2c04345c213ea0302e65289d824d1ebbe180
Johnatan Modesto <johnatan.modesto@gmail.com>
2020-09-14 23:10:13 -0300

    Applied DDD archtecture

---
+++ server/src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,11 @@
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import User from '../infra/typeorm/entities/User';
+
+interface IUsersRepository {
+  create(data: ICreateUserDTO): Promise<User>;
+  save(user: User): Promise<User>;
+  findById(id: string): Promise<User | undefined>;
+  findByEmail(email: string): Promise<User | undefined>;
+}
+
+export default IUsersRepository;
